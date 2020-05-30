caa6b252f0763fc4d282446e71a02fa4420e099f
Andres Morales <43222504+Testing42@users.noreply.github.com>
2020-05-30 17:12:19 -0400

    Add files via upload
    This shows how to use pub/jade or ejs.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var pug = require('pug');
 
 var port = 3000;
 
@@ -11,23 +12,43 @@ app.use(function(req, res, next) {
     next();
 })
 
+//app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public'))); // this shows static pages
 
 app.get('/', function(req, res) {
-    res.send('hello world!!!');
+    res.render('index', {
+        title: 'Hello World',
+        showTitle: true,
+        people: ['John', 'Andy', 'pat']
+    });
 });
 
 app.get('/about', function(req, res) {
     res.send('about page');
 });
 
-app.listen(3000);
-console.log('Server Started on port' + port);
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+app.get('/contact', function(req, res) {
+    res.render('contact');
+});
+
+app.listen(port);
+console.log('Server Started on port ' + port);
 
 module.exports = app;
 
 
 // command npm install nodemon -g installs this globally so you can run it anywhere
 // without reloading go to file and run nodemon
+
+
+// when looking for tutorials jade is same as pug
+//both are template engines that help make webapps
